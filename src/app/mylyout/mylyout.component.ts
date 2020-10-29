0c2dce9dd3fabc7f63b8bc30e07769c74fa3f099
AnteaterKit <ashatilovdev@gmail.com>
2020-10-30 00:48:34 +0300

    update

---
+++ src/app/mylyout/mylyout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-mylyout',
+  templateUrl: './mylyout.component.html',
+  styleUrls: ['./mylyout.component.scss']
+})
+export class MylyoutComponent {
+
+  constructor() {}
+
+}
